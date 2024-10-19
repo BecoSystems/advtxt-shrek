e67ff82fac0a305677455143e600c3be3426e325
Gilmar Lopes Da Silva Junior <glsj1@poli.br>
2024-10-19 17:12:19 -0300

    Função main já transcrita

--- exN.c
+++ exN.c
@@ -1,63 +1,3 @@
-/*****************************************************************************
- *   exN.c                                    Version 20180714.101818        *
- *                                                                           *
- *   Brief description                                                       *
- *   Copyright (C) 2016-2018    by Ruben Carlo Benante                       *
- *****************************************************************************
- *   This program is free software; you can redistribute it and/or modify    *
- *   it under the terms of the GNU General Public License as published by    *
- *   the Free Software Foundation; either version 2 of the License.          *
- *                                                                           *
- *   This program is distributed in the hope that it will be useful,         *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
- *   GNU General Public License for more details.                            *
- *                                                                           *
- *   You should have received a copy of the GNU General Public License       *
- *   along with this program; if not, write to the                           *
- *   Free Software Foundation, Inc.,                                         *
- *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.               *
- *****************************************************************************
- *   To contact the author, please write to:                                 *
- *   Ruben Carlo Benante                                                     *
- *   Email: rcb@beco.cc                                                      *
- *   Webpage: www.beco.cc                                                    *
- *   Phone: +55 (81) 3184-7555                                               *
- *****************************************************************************/
-
-/* ------------------------------------------------------------------------- */
-/**
- * @file exN.c
- * @ingroup GroupUnique
- * @brief Brief description
- * @details This program really do a nice job as a template, and template only!
- * @version 20160908.182830
- * @date 2016-09-08
- * @author Ruben Carlo Benante <<rcb@beco.cc>>
- * @par Webpage
- * <<a href="http://www.beco.cc">www.beco.cc</a>>
- * @copyright (c) 2016 GNU GPL v2
- * @note This program is free software: you can redistribute it
- * and/or modify it under the terms of the
- * GNU General Public License as published by
- * the Free Software Foundation version 2 of the License.
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * You should have received a copy of the GNU General Public License
- * along with this program.
- * If not, write to the Free Software Foundation, Inc.,
- * 59 Temple Place - Suite 330, Boston, MA. 02111-1307, USA.
- * Or read it online at <<http://www.gnu.org/licenses/>>.
- *
- *
- * @todo Now that you have the template, hands on! Programme!
- * @warning Be carefull not to lose your mind in small things.
- * @bug This file right now does nothing usefull
- *
- */
-
 /*
  * Instrucoes para compilar:
  *   $gcc exN.c -o exN.x -Wall
@@ -79,110 +19,38 @@
 /* ------------------------------------------------------------------------- */
 /* includes */
 
-#include <stdio.h> /* Standard I/O functions */
-#include <stdlib.h> /* Miscellaneous functions (rand, malloc, srand)*/
-#include <getopt.h> /* get options from system argc/argv */
-#include "exN.h" /* To be created for this template if needed */
+#include <stdio.h>
+#include<stdlib.h>
 
-/* #include <time.h> */ /* Time and date functions */
-/* #include <math.h> */ /* Mathematics functions */
-/* #include <string.h> */ /* Strings functions definitions */
-/* #include <dlfcn.h> */ /* Dynamic library */
-/* #include <malloc.h> */ /* Dynamic memory allocation */
-/* #include <unistd.h> */ /* UNIX standard function */
-/* #include <limits.h> */ /* Various C limits */
-/* #include <ctype.h> */ /* Character functions */
-/* #include <errno.h> */ /* Error number codes errno */
-/* #include <signal.h> */ /* Signal processing */
-/* #include <stdarg.h> */ /* Functions with variable arguments */
-/* #include <pthread.h> */ /* Parallel programming with threads */
-/* #include <fcntl.h> */ /* File control definitions */
-/* #include <termios.h> */ /* Terminal I/O definitions POSIX */
-/* #include <sys/stat.h> */ /* File status and information */
-/* #include <float.h> */ /* Float constants to help portability */
-/* #include <setjmp.h> */ /* Bypass standard function calls and return */
-/* #include <stddef.h> */ /* Various types and MACROS */
-/* #include <SWI-Prolog.h> */ /* Prolog integration with C */
-/* #include <ncurses.h> */ /* Screen handling and optimisation functions */
-/* #include <allegro.h> */ /* A game library for graphics, sounds, etc. */
-/* #include <libintl.h> */ /* Internationalization / translation */
-/* #include <locale.h> */ /* MACROS LC_ for location specific settings */
-/* #include "libeco.h" */ /* I/O, Math, Sound, Color, Portable Linux/Windows */
+void jogo();
+void jogo2();
+void menu();
 
-/* ------------------------------------------------------------------------- */
-/**
- * @ingroup GroupUnique
- * @brief This is the main event of the evening
- * @details Ladies and Gentleman... It's tiiiime!
- * Fightiiiiing at the blue corner,
- * he, who has compiled more C code than any other adversary in the history,
- * he, who has developed UNIX and Linux, and is an inspiration to maaany languages
- * and compilers, the GNU C Compiler, GCC!
- * Fightiiiiing at the red corner, the challenger, in his first fight, lacking of any
- * valid experience but angrily, blindly, and no doubtfully, will try to
- * compile this program without errors. He, the student, the apprentice,
- * the developer, beco!!
- *
- * @param[in] argc Argument counter
- * @param[in] argv Argument strings (argument values)
- *
- * @retval 0 If succeed (EXIT_SUCCESS).
- * @retval 1 Or another error code if failed.
- *
- * @par Example
- * @code
- *    $./exN -h
- * @endcode
- *
- * @warning   Be carefull with...
- * @bug There is a bug with...
- * @todo Need to do...
- * @note You can read more about it at <<a href="http://www.beco.cc">www.beco.cc</a>>
- * @author Ruben Carlo Benante
- * @version 20160908.182830
- * @date 2016-09-08
- *
- */
-int main(int argc, char *argv[])
+int main()
 {
-    int opt; /* return from getopt() */
-
-    IFDEBUG("Starting optarg loop...");
-
-    /* getopt() configured options:
-     *        -h  help
-     *        -V  version
-     *        -v  verbose
-     */
-    opterr = 0;
-    while((opt = getopt(argc, argv, "vhV")) != EOF)
-        switch(opt)
-        {
-            case 'h':
-                help();
-                break;
-            case 'V':
-                copyr();
-                break;
-            case 'v':
-                verb++;
-                break;
-            case '?':
-            default:
-                printf("Type\n\t$man %s\nor\n\t$%s -h\nfor help.\n\n", argv[0], argv[0]);
-                return EXIT_FAILURE;
-        }
-
-    if(verb)
-        printf("Verbose level set at: %d\n", verb);
-
-    exN_init(); /* initialization function */
-
-    /* ...and we are done */
-    /* Write your code here... */
-
-    return EXIT_SUCCESS;
-}
+    char select;
+//Primeiro  loop.
+//
+do{
+        printf("\n\n|SHREK|\n\n|THE ADVENTURE|\n\nSelecione 1 para abrir o menu e 2 para fechar :");
+        scanf(" %c",&select);
+    switch(select)
+    {
+        case '1':
+            menu();
+            break;
+        case '2':
+            printf("Encerrando ...");
+            break;
+        default :
+            while(select!='1'&&select!='2')
+            {
+                printf("Opção inválida, digite 1 ou 2 :");
+                scanf(" %c",&select);
+            }
+            break;
+    }
+    }while(select !='2');
 
 /* Write your functions here... */
 
@@ -197,21 +65,6 @@ int main(int argc, char *argv[])
  * @date 2016-09-08
  *
  */
-void help(void)
-{
-    IFDEBUG("help()");
-    printf("%s - %s\n", "exN", "Brief description");
-    printf("\nUsage: %s [-h|-v]\n", "exN");
-    printf("\nOptions:\n");
-    printf("\t-h,  --help\n\t\tShow this help.\n");
-    printf("\t-V,  --version\n\t\tShow version and copyright information.\n");
-    printf("\t-v,  --verbose\n\t\tSet verbose level (cumulative).\n");
-    /* add more options here */
-    printf("\nExit status:\n\t0 if ok.\n\t1 some error occurred.\n");
-    printf("\nTodo:\n\tLong options not implemented yet.\n");
-    printf("\nAuthor:\n\tWritten by %s <%s>\n\n", "Ruben Carlo Benante", "rcb@beco.cc");
-    exit(EXIT_FAILURE);
-}
 
 /* ------------------------------------------------------------------------- */
 /**
@@ -224,16 +77,6 @@ void help(void)
  * @date 2016-09-08
  *
  */
-void copyr(void)
-{
-    IFDEBUG("copyr()");
-    printf("%s - Version %s\n", "exN", VERSION);
-    printf("\nCopyright (C) %d %s <%s>, GNU GPL version 2 <http://gnu.org/licenses/gpl.html>. This  is  free  software: you are free to change and redistribute it. There is NO WARRANTY, to the extent permitted by law. USE IT AS IT IS. The author takes no responsability to any damage this software may inflige in your data.\n\n", 2016, "Ruben Carlo Benante", "rcb@beco.cc");
-    if(verb > 3) printf("copyr(): Verbose: %d\n", verb); /* -vvvv */
-    exit(EXIT_FAILURE);
-}
-
-/* ------------------------------------------------------------------------- */
 /**
  * @ingroup GroupUnique
  * @brief This function initializes some operations before start
@@ -273,13 +116,6 @@ void copyr(void)
  * @date 2016-09-08
  * @copyright Use this tag only if not the same as the whole file
  *
- */
-void exN_init(void)
-{
-    IFDEBUG("exN_init()");
-    /* initialization */
-    return;
-}
 
 /* ------------------------------------------------------------------------- */
 /* vi: set ai et ts=4 sw=4 tw=0 wm=0 fo=croql : C config for Vim modeline    */
